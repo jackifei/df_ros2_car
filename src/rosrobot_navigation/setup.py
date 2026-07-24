9fe5b0194f2425e833948989baf8edf1a9bfd9ae
AAAFei <793709242@qq.com>
2026-07-24 16:34:35 +0800

    修改bug

---
+++ src/rosrobot_navigation/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+
+package_name = 'rosrobot_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'maps'), glob('launch/*.yaml')),
+        (os.path.join('share', package_name, 'maps'), glob('config/*.pgm')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dengfei',
+    maintainer_email='793709242@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'rosrobot_nav = rosrobot_navigation.rosrobot_nav:main'
+        ],
+    },
+)
